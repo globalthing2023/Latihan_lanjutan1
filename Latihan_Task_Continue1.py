d591127cb10db203eb49099f953d3b02cb544c5f
globalthing2023 <140876287+globalthing2023@users.noreply.github.com>
2024-02-03 15:50:03 +0700

    Add files via upload

---
+++ Latihan_Task_Continue1.py
@@ -0,0 +1,37 @@
+Barang =  input( "Masukkan barang yang kamu mau beli: ")
+Harga = int ( input( "Masukkan juga harga barang tersebut: "))
+Jumlah = int ( input( "Berapa banyak barang tersebut kamu mau beli?: "))
+Bayar = int ( input( "Berapa kamu akan bayar untuk barang tersebut?: "))
+
+subTotal = Harga * Jumlah
+Diskon = subTotal * 0.2
+Total = subTotal - Diskon
+Kembalian = Bayar - Total
+Uang_50000 = Kembalian // 50000
+Kembalian = Kembalian - (50000 * Uang_50000)
+
+Uang_20000 = Kembalian // 20000
+Kembalian = Kembalian - (20000 * Uang_20000)
+
+Uang_10000 = Kembalian // 10000
+Kembalian = Kembalian - (10000 * Uang_10000)
+
+Uang_5000 = Kembalian // 5000
+Kembalian = Kembalian - (5000 * Uang_5000)
+
+Uang_2000 = Kembalian // 2000
+Kembalian = Kembalian - (2000 * Uang_2000)
+
+Uang_1000 = Kembalian // 1000
+Kembalian = Kembalian - (1000 * Uang_1000)
+
+print( f"SubTotal : {subTotal}")
+print( f"Diskon : {Diskon}")
+print( f"Total : {Total}")
+print( f"Kembalian : {Bayar - Total}")
+print( f"Kembalian_50000 : {Uang_50000}")
+print( f"Kembalian_20000 : {Uang_20000}")
+print( f"Kembalian_10000 : {Uang_10000}")
+print( f"Kembalian_5000 : {Uang_5000}")
+print( f"Kembalian_2000 : {Uang_2000}")
+print( f"Kembalian_1000 : {Uang_1000}")
